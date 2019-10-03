96babb000c2ad603c64ab7ac10ed9ac029762f5e
Elia Condorelli <elia.condorelli@gmail.com>
2019-10-03 23:03:45 +0100

    Small fixes to make the script work with the correct file names;

--- create_config.py
+++ create_config.py
@@ -1,21 +1,24 @@
-from jinja2 import Template
+import os
 import json
+from jinja2 import Template
 
+DEBUG = True
+output_dir = "output_configs"
 
+with open('ConfTemplate.j2', 'r') as fp:
+    loadtemplate = fp.read()
 
-with open('ConfTemplate.txt','r') as fp:
-    loadtemplate=fp.read()
+with open('Clients.json', 'r') as json_file:
+    clients = json.load(json_file)
 
-with open('Clients.txt') as json_file:
-    clients=json.load(json_file)
+jinjatemplate = Template(loadtemplate)
 
-jinjatemplate=Template(loadtemplate)
-
-
-for key in clients:
-    hostname=key
-    ip=clients[key]
-    conf=jinjatemplate.render(hostname=hostname,ip=ip)
-myfile=open(key+'.conf','w') #to move in the for loop
-result=myfile.writelines(conf)#to move in the for loop
+os.makedirs(output_dir, exist_ok=True)
+for hostname, ip in clients.items():
+    conf = jinjatemplate.render(hostname=hostname, ip=ip)
+    with open(os.path.join(output_dir, hostname + '.conf'), 'w') as myfile:
+        result = myfile.writelines(conf)
+    # Todo: Remove Debug code
+    if DEBUG:
+        break
 
